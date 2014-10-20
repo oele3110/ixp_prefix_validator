93830f13843aa2fd48881d3178dfa352441eda2c
tanger <oele3110@hotmail.com>
2014-10-20 14:08:49 +0200

    new table

---
+++ create_table_rs_rpki_roa.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `rs_rpki_roa` (
+  `rs_prefix_id` int(11) NOT NULL,
+  `asn` int(11),
+  `prefix` varchar(16),
+  `max` int(11),
+  `min` int(11),
+  PRIMARY KEY (`rs_prefix_id`,`asn`,`prefix`),
+  FOREIGN KEY (`rs_prefix_id`) REFERENCES `rs_prefixes` (`id`)
+)
