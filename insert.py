2bca999d3ac58e00e3fa1fd5c4055e4f7758a722
tanger <oele3110@hotmail.com>
2015-02-06 12:13:17 +0100

    updated - added settings.ini readin

--- insert.py
+++ insert.py
@@ -3,16 +3,26 @@ import datetime
 import time
 import re
 
-dbHost = 'localhost'
-dbUser = 'root'
-dbPw = 'skims12345'
-dbName = 'ixp'
+dbHost = None
+dbUser = None
+dbPw = None
+dbName = None
 
-def fetchUserAsn():
+def readIni(settingsFile):
+	ini = open(settingsFile)
+	settings = {}
+	for line in ini:
+		parts = re.match("(.*)=(.*)",line)
+		if parts != None:
+			settings[parts.group(1).strip()] = parts.group(2).strip()
+
+	return settings
+
+def fetchUserAsn(table):
 	user = {}
 	connection = mdb.connect(dbHost, dbUser, dbPw, dbName)
 	cursor = connection.cursor()
-	query = "SELECT `id`,`autsys` FROM `cust`"
+	query = "SELECT `id`,`autsys` FROM " + table
 	cursor.execute(query)
 	results = cursor.fetchall()
 
@@ -24,23 +34,24 @@ def fetchUserAsn():
 	return user
 
 
-def insert(users):
+def insert(users,table,filename):
 
 	connection = mdb.connect(dbHost, dbUser, dbPw, dbName)
 	cursor = connection.cursor()
 
 	pattern = "AS(\d{1,5})\|(.*)\|AS(\d{1,5})"
-	file = open("bgp-dump2.txt","r")
-	#counter = 0
+	file = open(filename,"r")
+	counter = 0
 
 	for line in file:
-		#counter +=1
-		#if counter%10000 == 0:
-		parts = re.match(pattern,line)
-		ts = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-		query = "INSERT INTO `rs_prefixes_out`(`custid`, `timestamp`, `prefix`, `protocol`, `irrdb`, `rs_origin`) VALUES ("+str(users[(int)(parts.group(1))])+",CURRENT_TIMESTAMP,'"+str(parts.group(2))+"',4,0,"+str(parts.group(3))+")"
-		#print query
-		cursor.execute(query)
+		counter +=1
+		# import only every 100th entry (for testing purposes)
+		if counter%100 == 0:
+			parts = re.match(pattern,line)
+			ts = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+			query = "INSERT INTO " + table + " (`custid`, `timestamp`, `prefix`, `protocol`, `irrdb`, `rs_origin`) VALUES ("+str(users[(int)(parts.group(1))])+",CURRENT_TIMESTAMP,'"+str(parts.group(2))+"',4,0,"+str(parts.group(3))+")"
+			#print query
+			cursor.execute(query)
 		pass
 		
 	connection.commit()
@@ -49,9 +60,20 @@ def insert(users):
 
 	return 0
 
+settings = readIni("settings.ini")
 
-users = fetchUserAsn()
-insert(users)
+dbHost = settings['dbHost']
+dbUser = settings['dbUser']
+dbPw = settings['dbPw']
+dbName = settings['dbName']
+
+print "fetcht user"
+users = fetchUserAsn(settings['custTable'])
+
+print "insert data into table"
+insert(users,settings['prefixesTable'],settings['dumpFile'])
+insert(users,settings['prefixesOutTable'],settings['dumpFile'])
+
 
 """
 for user in users:
